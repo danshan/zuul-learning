7cc4bef72db6da58c65ef96ae9e0056956459cc9
dan <i@shanhh.com>
2016-11-25 21:01:06 +0800

    feature init eureka

---
+++ zuul-gw-webapp/src/main/java/com/wanda/zuul/gw/webapp/ZuulServer.java
@@ -0,0 +1,16 @@
+package com.wanda.zuul.gw.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author dan
+ * @Since 2016-11-25 18:45
+ */
+@SpringBootApplication
+public class ZuulServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulServer.class, args);
+    }
+}
