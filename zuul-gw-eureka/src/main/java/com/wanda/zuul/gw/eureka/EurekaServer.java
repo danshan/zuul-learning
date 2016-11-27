95d1055f96fb320b94d80c2d115c47f437e3d2a3
dan <i@shanhh.com>
2016-11-27 09:37:59 +0800

    feature add turbine server

--- zuul-gw-eureka/src/main/java/com/wanda/zuul/gw/eureka/EurekaServer.java
+++ zuul-gw-eureka/src/main/java/com/wanda/zuul/gw/eureka/EurekaServer.java
@@ -1,14 +1,13 @@
 package com.wanda.zuul.gw.eureka;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
-@EnableEurekaServer
 @EnableHystrixDashboard
-@EnableTurbine
+@EnableEurekaServer
 public class EurekaServer {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer.class, args);
